c4b07c14e1da7c2faf77442daa0d937cd107c879
Philip Hale <phil989@gmail.com>
2013-11-16 19:06:08 +0000

    Tentatively added training to classifier - may belong elsewhere.

--- src/lib/classifier.rb
+++ src/lib/classifier.rb
@@ -1,7 +1,22 @@
 module EmotionClassifier
   class Classifier
+    def initialize(sentiments: sentiments)
+      @emotions = [].tap do |emotions|
+        sentiments.each { |sentiment| emotions << Emotion.new(:sentiment) }
+      end
+    end
+
     def classify(sentence: sentence)
+      train
       Emotion.new(:angry)
+    end
+
+    def train
+      @trained = true
+    end
+
+    def trained?
+      @trained
     end
   end
 end
--- src/spec/classifier_spec.rb
+++ src/spec/classifier_spec.rb
@@ -3,14 +3,23 @@ require 'spec_helper'
 module EmotionClassifier
   class ClassifierSpec
     describe Classifier do
+      let(:classifier) { Classifier.new(sentiments: [:testable]) }
+
+      it "should be untrained by default" do
+        classifier.trained?.should_not be_true
+      end
+
+      it "#train should train the classifier" do
+        classifier.train
+        classifier.trained?.should be_true
+      end
+
       it "should classify known angry-text as angry" do
-        classifier = Classifier.new
         emotion = classifier.classify(sentence: "I hate you!")
         emotion.should eq(Emotion.new(:angry))
       end
 
       xit "should classify known fearful-text as fearful" do
-        classifier = Classifier.new
         emotion = classifier.classify(sentence: "Please don't hurt me.")
         emotion.should eq(Emotion.new(:fearful))
       end
